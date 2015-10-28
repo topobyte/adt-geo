8020852c0e93509e765f2f8c8fdbab1f1cccdeff
Sebastian Kürten <sebastian.kuerten@fu-berlin.de>
2015-10-28 11:06:37 +0100

    Test for BBoxString
    Make sure that parse() and toString() are compatible.

--- src/test/java/de/topobyte/adt/geo/TestBBoxString.java
+++ src/test/java/de/topobyte/adt/geo/TestBBoxString.java
@@ -1,15 +1,20 @@
 package de.topobyte.adt.geo;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestBBoxString
 {
 
-	public static void main(String[] args)
+	@Test
+	public void test()
 	{
 		BBoxString box = BBoxString.parse("51.0,13,55.0,15.345345");
-		System.out.println(box.lon1);
-		System.out.println(box.lon2);
-		System.out.println(box.lat1);
-		System.out.println(box.lat2);
+
+		String asString = box.toString();
+		BBoxString parsed = BBoxString.parse(asString);
+
+		Assert.assertEquals(box.toBbox(), parsed.toBbox());
 	}
 
 }
